b84e1dcce35280a0e8d984149bfe19532034e949
txyang1 <137723945+txyang1@users.noreply.github.com>
2023-11-22 18:03:41 +0100

    NUE FILE

--- zf_mmseallocation.m
+++ zf_mmseallocation.m
@@ -7,16 +7,15 @@ function [ psi ] = zf_mmseallocation( phi, Ptx )
 % Ptx: available sum transmit power Ptx
 % Outputs
 % psi: vector of optimal power allocations psi1,...,psiN
+  N = length(phi);
+  psi = zeros(size(phi));
+  for i = 1:N
+       for  j = 1:N
+         psi(i) = Ptx/(sum(1/sqrt(phi(j)))*sqrt(phi(i)));
+       end 
+  end
+       
+%TODO
 
-sum_denom = 0; %The term on the denominator
-for j=1:numel(phi)
-    sum_denom = sum_denom + 1/sqrt(phi(j));
 end
 
-psi = zeros(size(phi)); %ready to save data
-
-for i=1:numel(phi)    
-    psi(i) = Ptx*(1/sqrt(phi(i)))/sum_denom;
-end
-
-%Team members: Tian Yu, Tingxin Yang
